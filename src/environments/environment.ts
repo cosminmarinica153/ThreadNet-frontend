bf4f3951a7af7a8de35ef49843a8665c9ae3b67c
cosminmarinica153 <marinica.cosmin2002@gmail.com>
2024-03-29 09:49:09 +0200

    environment change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrl: "https://threadnetbackendapi.azure-api.net/threadnet/api/"
 }
